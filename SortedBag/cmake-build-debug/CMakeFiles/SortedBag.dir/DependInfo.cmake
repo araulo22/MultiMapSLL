ba0c7b64b54769ab99bb4134f3277bd2a9682239
araulo22 <araulo22@gmail.com>
2022-03-20 16:44:12 +0200

    finished lab 1

---
+++ SortedBag/cmake-build-debug/CMakeFiles/SortedBag.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Uni stuff/DSA/SortedBag/App.cpp" "D:/Uni stuff/DSA/SortedBag/cmake-build-debug/CMakeFiles/SortedBag.dir/App.cpp.obj"
+  "D:/Uni stuff/DSA/SortedBag/ExtendedTest.cpp" "D:/Uni stuff/DSA/SortedBag/cmake-build-debug/CMakeFiles/SortedBag.dir/ExtendedTest.cpp.obj"
+  "D:/Uni stuff/DSA/SortedBag/ShortTest.cpp" "D:/Uni stuff/DSA/SortedBag/cmake-build-debug/CMakeFiles/SortedBag.dir/ShortTest.cpp.obj"
+  "D:/Uni stuff/DSA/SortedBag/SortedBag.cpp" "D:/Uni stuff/DSA/SortedBag/cmake-build-debug/CMakeFiles/SortedBag.dir/SortedBag.cpp.obj"
+  "D:/Uni stuff/DSA/SortedBag/SortedBagIterator.cpp" "D:/Uni stuff/DSA/SortedBag/cmake-build-debug/CMakeFiles/SortedBag.dir/SortedBagIterator.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ SortedBag/cmake-build-debug/CMakeFiles/SortedBag.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SortedBag.dir/App.cpp.obj"
+  "CMakeFiles/SortedBag.dir/ExtendedTest.cpp.obj"
+  "CMakeFiles/SortedBag.dir/ShortTest.cpp.obj"
+  "CMakeFiles/SortedBag.dir/SortedBag.cpp.obj"
+  "CMakeFiles/SortedBag.dir/SortedBagIterator.cpp.obj"
+  "SortedBag.exe"
+  "SortedBag.exe.manifest"
+  "SortedBag.pdb"
+  "libSortedBag.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SortedBag.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
